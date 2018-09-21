2edf1419f779bb79e810e86926377f6ff37df764
bpeterson1663 <bpeterson1663@gmail.com>
2018-09-20 23:31:43 -0500

    Edit working

--- src/components/GemListContainer/EditGem/EditGem.js
+++ src/components/GemListContainer/EditGem/EditGem.js
@@ -1,12 +1,68 @@
 import React, {Component} from 'react';
 import axios from '../../../axios';
+import Button from '../NewGem/NewForm/Button/Button';
+import FieldInput from '../NewGem/NewForm/FieldInput/FieldInput';
 
 class EditGem extends Component {
 
+    state = {
+        loadedGemstone: null
+    }
+
+    componentDidMount() {
+        this.getGemstone();
+    }
+    onUpdateHandler = () => {
+        const gemstone = {
+            name: this.state.name,
+            description: this.state.description,
+            id: this.props.match.params.id
+        }
+        this.updateGemstone(gemstone);
+    }
+
+    formChangeHandler = (event, param) => {
+        this.setState({
+            [param]: event.target.value
+        });
+    }
+    getGemstone () {
+        if (this.props.match.params.id){ 
+            axios.get('/gemstones/' + this.props.match.params.id + '.json')
+                .then(response => {
+                    this.setState({
+                        loadedGemstone: true,
+                        description: response.data.description,
+                        name: response.data.name
+                    })
+                })
+                .catch(error => {console.log("ERROR: ", error)});
+        }
+    }
+
+    updateGemstone (gemstone) {
+       
+        axios.put('/gemstones/' + gemstone.id + '.json', gemstone)
+            .then(response => {
+                console.log("Response: ", response);
+            })
+            .catch(error => {console.log("ERROR: ", error)});
+
+    }
     render () {
-        return (
-            <h1>Edit Gem</h1>
-        );
+        let gemstone = null;
+        console.log("PROPS: ", this.props);
+        if(this.state.loadedGemstone){
+            gemstone = (
+                <div>
+                    <FieldInput type="text" label="Name" value={this.state.name} changed={(event) => this.formChangeHandler(event, 'name')}/>
+                    <FieldInput type="text" label="Description" value={this.state.description} changed={(event) => this.formChangeHandler(event, 'description')}/>
+                    <Button label="Update" submit={this.onUpdateHandler} />               
+                </div>
+            );
+        }
+
+        return gemstone;
     }
 };
 
