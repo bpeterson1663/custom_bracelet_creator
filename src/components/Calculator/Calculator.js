bcf44b905c019b9a118ff2d4a32a62034473a009
bpeterson1663 <bpeterson1663@gmail.com>
2019-07-16 21:35:44 -0500

    Initial price calculator component

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from '../../axios';
+import BraceletContainer from './BraceletContainer/BraceletContainer';
 
-const Calculator = () => {
-    return(
-        <div>
-            <h1>Calculator</h1>
-        </div>
-    )
-};
+class Calculator extends Component {
+    state = {
+        gemstoneList: []
+    }
+    componentDidMount(){
+        this.getGemstoneList();
+    }
+    getGemstoneList = () => {
+        axios.get('/gemstones.json')
+            .then(res => {
+                const gemstones = [];
+
+                for (let key in res.data) {
+                    gemstones.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({
+                    gemstoneList: gemstones
+                });
+            })
+            .catch(error => {console.log("ERROR: ", error)});
+    }
+    render(){
+        return(
+            <div style={{
+                "padding": "20px",
+            }}>
+                <h1>Calculator</h1>
+                <BraceletContainer gemstoneList={this.state.gemstoneList}/>
+            </div>
+        )
+    }
+}
 
 export default Calculator;
